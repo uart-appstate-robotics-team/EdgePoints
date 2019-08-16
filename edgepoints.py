eb9029eaa9a85541c755ed0754b1bc2b2d45d8d2
Jacob Justice <jacobjustice@gmail.com>
2019-08-16 12:56:25 -0400

    module alterations

--- edgepoints.py
+++ edgepoints.py
@@ -1,15 +1,6 @@
 import cv2
 import numpy as nP
 
-img = cv2.imread('./small_flower.jpg')
-
-edges = cv2.Canny(img,200,250)
-
-cv2.imwrite('./output4.jpg',edges)
-print(edges.shape)
-#print("image",img)
-#print("edges",edges)
-
 def line_check(i,j,edge,check,lines,lno):
     if i-1 >= 0 and j-1 >=0:
         if  edge[i-1][j-1] > 0 and check[i-1][j-1] == False:
@@ -77,21 +68,24 @@ def line_check(i,j,edge,check,lines,lno):
     elif i+1 < len(edge) and j+1 < len(edge[i]):
         check[i+1][j+1] = True
 
-checked = [ [False for x in edges[0]] for x in edges]
+
 
 #print(checked)
 
+def generate_edgepoints(img):
+    edges = cv2.Canny(img,200,250)
 
-lineno = 0
-lines = [[]]
-for i in range(0,len(edges)):
-    for j in range(0,len(edges[i])):
-        if not checked[i][j]:
-            if edges[i][j] > 0:
-                line_check(i,j,edges,checked,lines,lineno)
-                lineno += 1
-                lines.append([])
-            else:
-                checked[i][j] = True
-
-print(lines)
+    checked = [ [False for x in edges[0]] for x in edges]
+    lineno = 0
+    lines = [[]]
+    for i in range(0,len(edges)):
+        for j in range(0,len(edges[i])):
+            if not checked[i][j]:
+                if edges[i][j] > 0:
+                    line_check(i,j,edges,checked,lines,lineno)
+                    lineno += 1
+                    lines.append([])
+                else:
+                    checked[i][j] = True
+    del lines[-1]
+    return lines
